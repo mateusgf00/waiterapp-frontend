c7ac1ea0fe2cb91c6fe078646645cbfbf9266fe4
Mateus <mateusgf00@gmail.com>
2023-03-22 20:15:29 -0300

    criado component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header"
 import { GlobalStyles } from "./styles/GlobalStyles"
 
 export function App() {
-  return <h1>Hello Word</h1>
+  return (
+    <>
+    <GlobalStyles />
+    <Header />
+    </>
+  )
 }
